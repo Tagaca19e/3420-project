5df4b11fd5d1b1b2fc1db4fafb99d47f40fae247
CecilioN7 <111897620+CecilioN7@users.noreply.github.com>
2022-12-11 15:43:24 -0800

    report

---
+++ snippets/user-report.php
@@ -0,0 +1,14 @@
+<?php
+echo "<div class='list-item__report'>
+        <h1>Seller Report</h1>
+        <p>Seller with the most listings:<br> $username</p>
+        <p>Number of Listings:<br> $listingcount</p>
+        <p>Users total:<br> $usernumber</p>
+        <h1>Listing Report</h1>
+        <p>Listings total:<br> $listingnum</p>
+        <p>Highest Price:<br> $$highestprice</p>
+        <p>Lowest Price:<br> $$lowestprice</p>
+        <p>Average book Price:<br> $$averageprice</p>
+
+    </div>"
+?>
