a92d091d2be35d03f975efba57e63b3c75168c5b
CecilioN7 <111897620+CecilioN7@users.noreply.github.com>
2022-12-11 15:42:55 -0800

    report

--- layout/report.php
+++ layout/report.php
@@ -24,39 +24,16 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] == false
     <body data-key="<?= encode($_SESSION["user_id"]) ?>">
         <div class="nav">
             <ul class="nav__primary-items">
-                <li class="nav__item" data-key="my-data">Account</li>
-                <li class="nav__item" data-key="create">Create</li>
-                <li class="nav__item" data-key="all-listings">Explore</li>
-                <li class="nav__item" data-key="my-listings">My Listings</li>
-                <li class="nav__item" data-key="report"> <a href="report.php">Report</a></li>
+                <li> <a href="dashboard.php">Home Page</a></li>
+                <li class="nav__item" data-key="report">View Report</li>
                 <li class="nav__item" data-key="logout">Logout</li>
             </ul>
         </div>
-
+        
         <!-- TODO(etagaca): Change names for classes. -->
         <div class="tradespace__wrapper">
-            <div class="tradespace__user-profile">
-                <h3><?= $_SESSION["username"] ?></h3>
-                <h4><?= get_user_rating($_SESSION["user_id"]) ?></h4>
-            </div>
-
             <!-- List of listings -->
-            <div class="tradespace__listing-wrapper"></div>
-
-            <div class="tradespace__message-wrapper">
-                <!-- List of messaged users -->
-                <div class="tradespace__message-list"></div>
-
-                <div class="tradespace__messages">
-                    <!-- Main messages -->
-                    <div class="tradespace__messages-inner"></div>
-                    <input 
-                        type="text" 
-                        id="message-text" 
-                        placeholder="Enter message"
-                    >
-                </div>
-            </div>
+            <div class="tradespace__report-wrapper"></div>
         </div>
 
         <!-- Load apis -->
